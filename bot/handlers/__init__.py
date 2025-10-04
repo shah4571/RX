4ee3b2a08edabf2c13ad92bf07c1804a28e5e4d8
shah4571 <shahriya4571@gmail.com>
2025-10-05 03:19:44 +0600

    __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from .start import register_start
 from .cap import register_cap
 from .account import register_account
@@ -7,14 +7,15 @@ from .withdraw import register_withdraw
 from .support import register_support
 from .admin import register_admin
 
+# Register all bot handlers
 def init_handlers(app: Client):
     """
     Register all bot handlers
     """
-    # Using the filters.command with proper callback function
-    app.add_handler(filters.command("start")(register_start))
-    app.add_handler(filters.command("cap")(register_cap))
-    app.add_handler(filters.command("account")(register_account))
-    app.add_handler(filters.command("withdraw")(register_withdraw))
-    app.add_handler(filters.command("support")(register_support))
-    app.add_handler(filters.command("admin")(register_admin))
+    # Use lambda to properly pass client and message parameters
+    app.add_handler(filters.command("start")(lambda client, message: register_start(client, message)))
+    app.add_handler(filters.command("cap")(lambda client, message: register_cap(client, message)))
+    app.add_handler(filters.command("account")(lambda client, message: register_account(client, message)))
+    app.add_handler(filters.command("withdraw")(lambda client, message: register_withdraw(client, message)))
+    app.add_handler(filters.command("support")(lambda client, message: register_support(client, message)))
+    app.add_handler(filters.command("admin")(lambda client, message: register_admin(client, message)))
