52aec43eb482b6ec92e5eb8da27bccc7140e43c8
shah4571 <shahriya4571@gmail.com>
2025-10-05 02:37:12 +0600

    __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,8 +1,6 @@
 # bot/handlers/__init__.py
 
-# Import all handler registration functions here
-from .start import register_start  # Make sure this line is added
-
+from .start import register_start
 from .cap import register_cap
 from .account import register_account
 from .withdraw import register_withdraw
@@ -16,13 +14,13 @@ def init_handlers(updater: Updater):
     """
     dp = updater.dispatcher
     
-    # Register the handlers
-    dp.add_handler(CommandHandler("start", register_start))  # No group=0 now
-    dp.add_handler(CommandHandler("cap", register_cap))
-    dp.add_handler(CommandHandler("account", register_account))
-    dp.add_handler(CommandHandler("withdraw", register_withdraw))
-    dp.add_handler(CommandHandler("support", register_support))
-    dp.add_handler(CommandHandler("admin", register_admin))
+    # Register the handlers with the group=0 argument
+    dp.add_handler(CommandHandler("start", register_start, group=0))  # Add group=0
+    dp.add_handler(CommandHandler("cap", register_cap, group=0))
+    dp.add_handler(CommandHandler("account", register_account, group=0))
+    dp.add_handler(CommandHandler("withdraw", register_withdraw, group=0))
+    dp.add_handler(CommandHandler("support", register_support, group=0))
+    dp.add_handler(CommandHandler("admin", register_admin, group=0))
 
     # Register the callback query handler for menu options
     dp.add_handler(CallbackQueryHandler(show_menu, pattern="menu_options"))
