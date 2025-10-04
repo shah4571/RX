38ab44fd777fa4f599a7d87707a2f5f76c26b403
shah4571 <shahriya4571@gmail.com>
2025-10-05 02:34:18 +0600

    __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,3 +1,13 @@
+# bot/handlers/__init__.py
+
+# Import all handler registration functions here
+from .start import register_start  # Make sure this line is added
+
+from .cap import register_cap
+from .account import register_account
+from .withdraw import register_withdraw
+from .support import register_support
+from .admin import register_admin
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 def init_handlers(updater: Updater):
@@ -6,8 +16,8 @@ def init_handlers(updater: Updater):
     """
     dp = updater.dispatcher
     
-    # Register the handlers without the 'group' argument
-    dp.add_handler(CommandHandler("start", register_start))  # Removed group=0
+    # Register the handlers
+    dp.add_handler(CommandHandler("start", register_start))  # No group=0 now
     dp.add_handler(CommandHandler("cap", register_cap))
     dp.add_handler(CommandHandler("account", register_account))
     dp.add_handler(CommandHandler("withdraw", register_withdraw))
@@ -15,4 +25,4 @@ def init_handlers(updater: Updater):
     dp.add_handler(CommandHandler("admin", register_admin))
 
     # Register the callback query handler for menu options
-    dp.add_handler(CallbackQueryHandler(show_menu, pattern="menu_options"))  # Added for menu interaction
+    dp.add_handler(CallbackQueryHandler(show_menu, pattern="menu_options"))
